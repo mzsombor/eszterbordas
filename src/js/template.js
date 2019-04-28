32716803ef943d52f8f3db80b541754a4226a135
zsombor <zsombro@ibrowse.com>
2019-04-28 21:31:57 +0300

    wip

--- src/js/template.js
+++ src/js/template.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         //either specify lenght which goes from 0 -> length
         //or specify "order": ["img_0003.jpg", "imig_001.jpg"]
     var gallery = {
-	"Sweden": {
+    "Sweden": {
             "title": "Sweden",
             "length": "1",
             "content": ["eszter bordas", "photography", "minimal", "malmo", "sweden", "sea"]
@@ -101,35 +101,10 @@ $(document).ready(function() {
             "length": "9",
             "content": ["Bajko Renata 2","Bajko Renata 2"]
         },
-        "Viski_Orsi": {
-            "title": "Viski Orsi",
-            "length": "18",
-            "content": ["Viski Orsi","Viski Orsi"]
-        },
         "Sea": {
             "title": "Sea",
             "length": "17",
             "content": ["Sea","Sea"]
-        },
-        "Gergely_Orsi": {
-            "title": "Gergely Orsi",
-            "length": "7",
-            "content": ["Gergely Orsi","Gergely Orsi"]
-        },
-        "Kispal_Timea": {
-            "title": "Kispal Timea",
-            "length": "12",
-            "content": ["Kispal Timea","Kispal Timea"]
-        },
-        "Bajko_Renata": {
-            "title": "Bajko Renata",
-            "length": "8",
-            "content": ["Bajko Renata","Bajko Renata"]
-        },
-        "Bajko_Reni": {
-            "title": "Bajko Renata",
-            "length": "9",
-            "content": ["Bajko Reni","Autumn"]
         }
     }
     var pad = "0000"
